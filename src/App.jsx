eb49c0df38f6d309b90dfcf48c713e7b60a916db
Danasawin <danasawin-eng@hotmail.com>
2025-06-10 19:01:24 +0700

    fixthing

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>haha</h1>
+      <h1>haha Fix thing</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
