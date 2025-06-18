db3b346b78c1bb81261f4131b685c2fd8ceb7d63
Danasawin <danasawin-eng@hotmail.com>
2025-06-18 12:48:15 +0700

    6/18 and text TDGs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Test docker and jenkins in GCPs with docker compose</h1>
-      <h1>TDG</h1>
+      <h1>TDGs</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
